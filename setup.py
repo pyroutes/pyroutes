e30dcc71184259864db2c2a51aa2123399faf721
xim <xim-git@akuma.no>
2012-03-04 15:14:42 +0100

    Reorder some stuff in setup.py

--- setup.py
+++ setup.py
@@ -11,18 +11,20 @@ except ImportError:
 
 import os
 
+desc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
 setup(
     name = "pyroutes",
     version = "0.4.1",
-    url = 'http://github.com/pyroutes/pyroutes',
-    license = 'GPLv2',
-    description = "A small WSGI wrapper for creating small python web apps",
-    long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    author = 'Kristian Klette',
     author_email = 'klette@samfundet.no',
-    packages = ['pyroutes','pyroutes.http', 'pyroutes.template', 'pyroutes.contrib', 'pyroutes.middleware'],
-    requires = ['wsgiref'],
+    author = 'Kristian Klette',
+    description = "A small WSGI wrapper for creating small python web apps",
+    license = 'GPLv2',
+    long_description = desc,
+    url = 'http://github.com/pyroutes/pyroutes',
     package_data = {'pyroutes': ['default_templates/*.xml', 'default_templates/fileserver/*.xml']},
+    packages = ['pyroutes', 'pyroutes.http', 'pyroutes.template', 'pyroutes.contrib', 'pyroutes.middleware'],
+    requires = ['wsgiref'],
     scripts = ['bin/pyroutes-admin.py'],
     cmdclass = {'build_py': build_py},
     classifiers = [
